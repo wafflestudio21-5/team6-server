eb91d0bddb564a51ec0e61c321823a6a977aefb3
nuagenic <nuagenic@gmail.com>
2024-01-05 20:15:00 +0900

    fix/Modify 'wsgi.py' for deployment

--- WafflePedia/WafflePedia/wsgi.py
+++ WafflePedia/WafflePedia/wsgi.py
@@ -7,10 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WafflePedia.settings')
+# add the WafflePedia project path into the sys.path
+project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_path)
+
+# add the virtualenv site-packages path to the sys.path
+virtualenv_path = os.path.join(
+    project_path, "venv", "lib", "python3.9", "site-packages"
+)
+sys.path.append(virtualenv_path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WafflePedia.settings")
 
 application = get_wsgi_application()
