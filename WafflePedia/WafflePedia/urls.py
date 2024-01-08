0c91ff8af62ba53fcb5fa9d874f8cff4b9f8592e
nuagenic <nuagenic@gmail.com>
2024-01-08 20:22:47 +0900

    fix/Modify urls.py(임시)

--- WafflePedia/WafflePedia/urls.py
+++ WafflePedia/WafflePedia/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("sample/", include("sample.urls")),
+    path("sample/", include("WafflePedia.sample.urls")),
 ]
