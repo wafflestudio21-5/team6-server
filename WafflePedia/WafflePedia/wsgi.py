3c38f320a892933c2bd58cbaf9ca5a4b410a66be
nuagenic <nuagenic@gmail.com>
2024-01-08 20:10:20 +0900

    fix/Modify 'wsgi.py' for deployment

--- WafflePedia/WafflePedia/wsgi.py
+++ WafflePedia/WafflePedia/wsgi.py
@@ -11,6 +11,6 @@ import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WafflePedia.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WafflePedia.WafflePedia.settings")
 
 application = get_wsgi_application()
