e47360f9797f6380824a38ceb7a8b73eae3c485b
Yi Daeun <dawnayi2002@gmail.com>
2024-01-08 20:42:33 +0900

    feat/add token view

--- WafflePedia/waffleAuth/views.py
+++ WafflePedia/waffleAuth/views.py
@@ -1,3 +1,73 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.views import APIView
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import *
+from .models import WaffleUser
+
+import os
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+from django.http import JsonResponse
+
+
+class CookieTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response_data = super().post(request, *args, **kwargs).data
+        access_token = response_data.get("access")
+        refresh_token = response_data.get("refresh")
+
+        # Modify the response data to only include the access token
+        response_data = {"access": access_token}
+        response = JsonResponse(response_data)
+        if refresh_token:
+            response.set_cookie(
+                "refresh_token",
+                refresh_token,
+                httponly=True,
+                samesite="None",
+            )
+
+        return response
+
+
+class CookieTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        response_data = super().post(request, *args, **kwargs).data
+        access_token = response_data.get("access")
+        refresh_token = response_data.get("refresh")
+
+        # Modify the response data to only include the access token
+        response_data = {"access": access_token}
+        response = JsonResponse(response_data)
+
+        if refresh_token:
+            response.set_cookie(
+                "refresh_token",
+                refresh_token,
+                httponly=True,
+                samesite="None",
+            )
+
+        return response
+
+
+# authentication test
+class MyProtectedView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        print("Headers:", request.headers)
+        response = JsonResponse({"message": "You are authenticated"})
+        print("Response:", response.content.decode())
+        return response
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = WaffleUser.objects.all()
+    serializer_class = UserSerializer
