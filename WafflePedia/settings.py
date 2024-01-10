01174ebbf0044ef69bb7e1440c0b17db5886ecad
civilian38 <gyk23953@gmail.com>
2024-01-10 23:10:32 +0900

    fix/roll back LANGUAGE_CODE, TIME_ZONE

--- WafflePedia/settings.py
+++ WafflePedia/settings.py
@@ -182,9 +182,9 @@ ACCOUNT_EMAIL_VERIFICATION = "none"
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'Asia/Seoul'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
