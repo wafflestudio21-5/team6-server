fabee07eb59164a18504309f237a5f809fd4abf0
woojin-blaze <142714911+woojin-blaze@users.noreply.github.com>
2024-01-12 17:56:59 +0900

    feat/serializers

---
+++ userprofile/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import urls
+from .views import *
+
+
+urlpatterns = [
+    path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('<int:user_id>/followers/', FollowersListView.as_view(), name='user-followers'),
+    path('<int:user_id>/followings/', FollowingsListView.as_view(), name='user-following'),
+    path('<int:pk>/comments/', UserCommentsListView.as_view(), name='user-comments'),
+    path('<int:user_id>/ratings/', UserRatingListView.as_view(), name='user-ratings'),
+    path('<int:user_id>/movies/watching/', UserMovieStateListView.as_view(), {'state': 'watching'}, name='user-watching-movies'),
+    path('<int:user_id>/movies/want_to_watch/', UserMovieStateListView.as_view(), {'state': 'want_to_watch'}, name='user-want-to-watch-movies'),
+    path('<int:user_id>/movies/not_interested/', UserMovieStateListView.as_view(), {'state': 'not_interested'}, name='user-not-interested-movies'),
+
+    ]
