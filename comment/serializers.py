f574735f6ea44b4cf8454b67cd734f1907836786
civilian38 <gyk23953@gmail.com>
2024-01-14 13:41:46 +0900

    fix/improve commentListSerializer
    now include writer's information, value of rate for corresponding review

--- comment/serializers.py
+++ comment/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
+
 from .models import *
+from content.models import Rating
+from waffleAuth.models import WaffleUser
+
+
+class CommentWriterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WaffleUser
+        fields = ['id', 'nickname', 'profile_photo']
+
+
+class CommentRatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ['rate']
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    created_by = CommentWriterSerializer(read_only=True)
+    rating = CommentRatingSerializer(read_only=True)
+
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ['id', 'created_by', 'content', 'has_spoiler', 'created_at', 'updated_at', 'movie', 'rating']
--- comment/urls.py
+++ comment/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('<int:pk>', CommentListAPIView.as_view()),
+    path('<str:pk>', CommentListAPIView.as_view()),
 ]
