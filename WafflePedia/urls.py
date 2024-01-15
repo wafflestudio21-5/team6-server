f990459ebff0f747d2c8783eef376f60a5537fb4
woojin-blaze <142714911+woojin-blaze@users.noreply.github.com>
2024-01-15 22:48:02 +0900

    fix/sample connection check 수정

--- WafflePedia/urls.py
+++ WafflePedia/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("sample/", include("sample.urls")),
+    path("api/sample/", include("sample.urls")),
     path("auth/", include("waffleAuth.urls")),
 ]
