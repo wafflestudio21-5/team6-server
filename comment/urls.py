0f296d633ee056e1a476758848bc36a026593ccc
civilian38 <gyk23953@gmail.com>
2024-01-17 00:04:21 +0900

    fix/modify url structure

--- comment/urls.py
+++ comment/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('<str:pk>', CommentListAPIView.as_view()),
+    path('', CommentListAPIView.as_view()),
 ]
--- content/urls.py
+++ content/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
     path('', MovieListAPI.as_view()),
     path('<str:pk>', MovieRetrieveAPI.as_view()),
+    path('<str:pk>/comments/', include('comment.urls')),
     path('<str:pk>/rate', RatingAPI.as_view()),
 ]
