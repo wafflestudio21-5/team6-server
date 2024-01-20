43c40e1ce3e37363d1f5ec4827f24bf46e6ce908
Yi Daeun <dawnayi2002@gmail.com>
2024-01-20 18:49:02 +0900

    feat/create serializers.py and set views

--- search/views.py
+++ search/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+from waffleAuth.models import WaffleUser
+from content.models import Movie
+
+from rest_framework.generics import ListAPIView
+
+class UserSearchListView(ListAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs.get("user_id")
+        user = WaffleUser.objects.get(pk=user_id)
+        return user.following.all()
+
+
+class MovieSearchListView(ListAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs.get("user_id")
+        user = WaffleUser.objects.get(pk=user_id)
+        return user.following.all()
+
+class KeywordSearchListView(ListAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs.get("user_id")
+        user = WaffleUser.objects.get(pk=user_id)
+        return user.following.all()
